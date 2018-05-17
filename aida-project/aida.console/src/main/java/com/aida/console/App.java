d93d60f5a21f240249d17bece393f5f98b03c603
Daqing Feng <fengdaqing@hotmail.com>
2018-05-17 20:50:39 +0800

    rabbitmq realize
    rabbitmq realize.

--- aida-project/aida.console/src/main/java/com/aida/console/App.java
+++ aida-project/aida.console/src/main/java/com/aida/console/App.java
@@ -20,7 +20,7 @@ public class App {
 
     static void Producer() {
         try {
-            RabbitmqProducer producer = new RabbitmqProducer("amqp://sc:Yiguo%40sc@172.17.7.207:5672/sc");
+            RabbitmqProducer producer = new RabbitmqProducer("amqp://sc:Yiguo%40sc@127.0.0.1:5672/sc");
             producer.setExchange("ex_commodity");
             producer.setQueueName("queue_stock_test");
             producer.SendMessage("Hello World");
@@ -31,7 +31,7 @@ public class App {
 
     static void Comsumer() {
         try {
-            RabbitmqConsumer producer = new RabbitmqConsumer("amqp://sc:Yiguo%40sc@172.17.7.207:5672/sc");
+            RabbitmqConsumer producer = new RabbitmqConsumer("amqp://sc:Yiguo%40sc@127.0.0.1:5672/sc");
             producer.setExchange("ex_commodity");
             producer.setQueueName("queue_stock_test");
             producer.Receive(App::GetMessage);
