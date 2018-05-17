7aaa1b6de82fbda827ba16d3e1f943133b398d71
Daqing Feng <fengdaqing@hotmail.com>
2018-05-17 20:48:15 +0800

    add console packing.
    add conosle packing pom.xml file

--- aida-project/aida.console/src/main/java/com/aida/console/App.java
+++ aida-project/aida.console/src/main/java/com/aida/console/App.java
@@ -7,12 +7,18 @@ import com.aida.util.rabbitmq.RabbitmqProducer;
  * Created by fengdaqing on 2018/2/24.
  */
 public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Begin the Consumer");
+        Comsumer();
+    }
+
     public static String GetMessage(String message) {
         System.out.println(String.format("Received message : %s", message));
         return message;
     }
 
-      static void Producer() {
+    static void Producer() {
         try {
             RabbitmqProducer producer = new RabbitmqProducer("amqp://sc:Yiguo%40sc@172.17.7.207:5672/sc");
             producer.setExchange("ex_commodity");
@@ -23,7 +29,7 @@ public class App {
         }
     }
 
-      static void Comsumer() {
+    static void Comsumer() {
         try {
             RabbitmqConsumer producer = new RabbitmqConsumer("amqp://sc:Yiguo%40sc@172.17.7.207:5672/sc");
             producer.setExchange("ex_commodity");
@@ -34,8 +40,5 @@ public class App {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Begin the Consumer");
-        Comsumer();
-    }
+
 }
